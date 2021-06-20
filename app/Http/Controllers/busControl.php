2620e4b3148ee82d3700149a48f78e72827c9aaa
deshanviha <it19106670@my.sliit.lk>
2021-06-21 03:45:34 +0530

    bus manage complete api

---
+++ app/Http/Controllers/busControl.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Routing\Controller;
+use App\Models\Buses;
+
+class busControl extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return Buses::all();
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $request ->validate([
+           'name' => 'required',
+           'type' => 'required',
+            'vehicle_number' => 'required'
+        ]);
+        return Buses::create($request->all());
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        return Buses::find($id);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $bus = Buses::find($id);
+        $bus -> update ($request->all());
+
+        return $bus;
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        return Buses::destroy($id);
+    }
+
+    /**
+     * Search bus.
+     *
+     * @param  str  $name
+     * @return \Illuminate\Http\Response
+     */
+    public function search($name)
+    {
+        return Buses::where('name', 'like' ,'%'.$name.'%')->get();
+    }
+
+}
--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,8 @@
 <?php
-
+use App\Http\Controllers\busControl;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -13,7 +14,15 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+//Route::get('/buses' ,[busControl::class, 'index']);
+//Route::post('/buses',[busControl::class, 'store']);
+Route::get('/buses/search/{name}',[busControl::class, 'search']);
+
+Route::resource('buses', busControl::class);
+
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
